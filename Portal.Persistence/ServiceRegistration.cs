51d177c79d4c0616a082713ad5bf5118c6241829
Berke Yıldırım <berke-yildirim-2580@hotmail.com>
2023-08-29 10:51:58 +0300

    connection string ayarı

--- Portal.Persistence/ServiceRegistration.cs
+++ Portal.Persistence/ServiceRegistration.cs
@@ -4,30 +4,42 @@ using Portal.Application.Repositories;
 using Portal.Persistence.Context;
 using Portal.Persistence.Repositories;
 using BackGroundService;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace ControlPortal.Persistence
 {
     public static class ServiceRegistiration
     {
-
         public static void AddPersistenceServices(this IServiceCollection services)
         {
 
-            services.AddDbContext<PortalDbContext>(options => options.UseSqlServer("Server=10.10.10.60;Database=PortalDb;User Id=sa;Password=123456;TrustServerCertificate=True;"));
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var jsonFilePath = Path.Combine(currentDirectory, "appsettings.json");// JSON dosyasının yolunu belirtin
+            using (StreamReader file = File.OpenText(jsonFilePath))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JToken jToken = JToken.ReadFrom(reader);
 
-            services.AddScoped<IEtkinlikReadRepository, EtkinlikReadRepository>();
-            services.AddScoped<IEtkinlikWriteRepository, EtkinlikWriteRepository>();
+                // JSON içeriği üzerinde işlemler yapabilirsiniz
+                var connectionStrings = jToken["ConnectionStrings"];
+                var sqlServerConnectionString = connectionStrings["SqlServer"].ToString();
 
-            services.AddScoped<IUserReadRepository, UserReadRepository>();
-            services.AddScoped<IUserWriteRepository, UserWriteRepository>();
+                services.AddDbContext<PortalDbContext>(options => options.UseSqlServer(sqlServerConnectionString));
 
-            services.AddScoped<IAccessTokenWriteRepository, AccessTokenWriteRepository>();
-            services.AddScoped<IAccessTokenReadRepository, AccessTokenReadRepository>();
+                services.AddScoped<IEtkinlikReadRepository, EtkinlikReadRepository>();
+                services.AddScoped<IEtkinlikWriteRepository, EtkinlikWriteRepository>();
 
-            services.AddSingleton<MyBackgroundService>();
-            services.AddHostedService(provider => provider.GetRequiredService<MyBackgroundService>());
+                services.AddScoped<IUserReadRepository, UserReadRepository>();
+                services.AddScoped<IUserWriteRepository, UserWriteRepository>();
 
+                services.AddScoped<IAccessTokenWriteRepository, AccessTokenWriteRepository>();
+                services.AddScoped<IAccessTokenReadRepository, AccessTokenReadRepository>();
+
+                services.AddSingleton<MyBackgroundService>();
+                services.AddHostedService(provider => provider.GetRequiredService<MyBackgroundService>());
+
+            }
         }
-
     }
 }
